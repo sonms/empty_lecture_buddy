bf2ae4dc894d3f796475dcd6fcf421a8167ee0d5
smsdia412 <smsdia412@gmail.com>
2025-05-28 02:04:48 +0000

    setting/gitignore, package, config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // 클라이언트에서 /api/* 로 요청하면
+      '/api': {
+        target: 'https://0adb-221-142-225-145.ngrok-free.app', 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
+    },
+  },
+});
